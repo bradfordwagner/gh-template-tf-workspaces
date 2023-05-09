0c45bd628da4054b43a3e2fe80d481ab07a2b1a8
bradfordwagner <wagner.bradford@gmail.com>
2023-05-08 23:27:18 -0400

    cleanup

--- main.tf
+++ main.tf
@@ -1,39 +1,3 @@
-# provider "azurerm" {
-#   features {}
-# }
-
-
-# resource group for all infrastructure to be added to
-# resource "azurerm_resource_group" "rg" {
-#   name     = "bradfordwagner-infra"
-#   location = "eastus2"
-# }
-
-# # create a kubernetes cluster
-# module "k8s" {
-#   depends_on          = [azurerm_resource_group.rg]
-#   source              = "./modules/k8s_cluster"
-#   enabled             = false
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
-
-# # create a blub storage
-# module "blob_storage" {
-#   depends_on          = [azurerm_resource_group.rg]
-#   source              = "./modules/blob_storage"
-#   enabled             = false
-#   resource_group_name = azurerm_resource_group.rg.name
-#   name                = "blubblubblub"
-# }
-
-# module "keyvault" {
-#   depends_on          = [azurerm_resource_group.rg]
-#   source              = "./modules/keyvault"
-#   enabled             = false
-#   resource_group_name = azurerm_resource_group.rg.name
-#   name                = "bradfordwagner-test1"
-# }
-
 module "hello_world" {
   source = "git::https://github.com/bradfordwagner/gh-template-tf-module.git?ref=1.0.0"
   input  = "test test"
