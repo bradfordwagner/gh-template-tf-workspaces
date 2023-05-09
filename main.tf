886f331a72762167583defc55ef806299c7c6b45
bradfordwagner <wagner.bradford@gmail.com>
2023-05-08 23:16:00 -0400

    add output

--- main.tf
+++ main.tf
@@ -38,3 +38,8 @@ module "hello_world" {
   source = "git::https://github.com/bradfordwagner/gh-template-tf-module.git?ref=1.0.0"
   input  = "test test"
 }
+
+output "name" {
+  value = module.hello_world.hello_world
+}
+
