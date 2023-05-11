bee400f83224e318354fa1c0f6b5718d045ba0a5
bradfordwagner <wagner.bradford@gmail.com>
2023-05-10 22:58:59 -0400

    add tst

--- environments/tst/main.tf
+++ environments/tst/main.tf
@@ -1,6 +1,10 @@
+terraform {
+  backend "azurerm" {}
+}
+
 module "hello_world" {
   source = "git::https://github.com/bradfordwagner/gh-template-tf-module.git?ref=1.0.0"
-  input  = "test test"
+  input  = var.input
 }
 
 output "name" {
